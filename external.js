8f26591cec08df3e871575d5096f658428e8176e
BananaWafflez <benaing12@gmail.com>
2023-08-11 09:47:57 -0400

    Centered grid

--- external.js
+++ external.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 const grid = document.querySelector('.grid');
 for(let i = 0; i < 256; i++){
     const square = document.createElement('div');
+    square.classList.add("square");
     grid.appendChild(square);
-    square.textContent = "test";
+    square.textContent = `${i}`;
 }
