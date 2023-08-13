f71eadae86758e20ae2476569e5761a1e33074dd
BananaWafflez <benaing12@gmail.com>
2023-08-13 18:19:40 -0400

    Square changes color when mouse hovers over square

--- external.js
+++ external.js
@@ -1,12 +1,20 @@
+function changeColor(event){
+    const select = event.target;
+    select.classList.add("black");
+}
+
+
+
 const body = document.querySelector('body');
 const grid = document.querySelector('.grid');
 let size = 17.3;
-let numofsquare = 100;
+let numofsquare = 16;
 grid.style.cssText = `max-width: ${size * numofsquare}px`
 for(let i = 0; i < numofsquare; i++){
     for(let j = 0; j < numofsquare; j++){
         const square = document.createElement('div');
         square.classList.add("square");
+        square.addEventListener('mouseover',changeColor);
         grid.appendChild(square);
     }
     const linebreak = document.createElement('br');
