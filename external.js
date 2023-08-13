be0a39e0b40f5656b84d79fb4212398bb3aad10f
BananaWafflez <benaing12@gmail.com>
2023-08-13 09:48:00 -0400

    Made grid dynamically size depending on amount of squares

--- external.js
+++ external.js
@@ -1,9 +1,15 @@
 const body = document.querySelector('body');
-
 const grid = document.querySelector('.grid');
-for(let i = 0; i < 256; i++){
-    const square = document.createElement('div');
-    square.classList.add("square");
-    grid.appendChild(square);
+let size = 33;
+let numofsquare = 16;
+grid.style.cssText = `max-width: ${size * numofsquare}px`
+for(let i = 0; i < numofsquare; i++){
+    for(let j = 0; j < numofsquare; j++){
+        const square = document.createElement('div');
+        square.classList.add("square");
+        grid.appendChild(square);
+    }
+    const linebreak = document.createElement('br');
+    //grid.appendChild(linebreak);
     //square.textContent = `${i}`;
 }
