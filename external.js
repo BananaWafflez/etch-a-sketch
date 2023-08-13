336136be54cfc18ebd57fc839bc15989401d6fed
BananaWafflez <benaing12@gmail.com>
2023-08-13 10:06:39 -0400

    Altered square values to be more precise

--- external.js
+++ external.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body');
 const grid = document.querySelector('.grid');
-let size = 33;
-let numofsquare = 16;
+let size = 17.3;
+let numofsquare = 100;
 grid.style.cssText = `max-width: ${size * numofsquare}px`
 for(let i = 0; i < numofsquare; i++){
     for(let j = 0; j < numofsquare; j++){
