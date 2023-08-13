4435d889aa5bab542a4133070c51b357811bc7fd
BananaWafflez <benaing12@gmail.com>
2023-08-13 09:10:19 -0400

    Made boxes into 16x16 grid

--- external.js
+++ external.js
@@ -5,5 +5,5 @@ for(let i = 0; i < 256; i++){
     const square = document.createElement('div');
     square.classList.add("square");
     grid.appendChild(square);
-    square.textContent = `${i}`;
+    //square.textContent = `${i}`;
 }
