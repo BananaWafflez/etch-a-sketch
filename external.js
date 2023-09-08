b3d0fe442197db43d698f3e2bab012f09601e04b
BananaWafflez <benaing12@gmail.com>
2023-09-08 13:36:25 -0400

    Overhauling js square implementation, wip

--- external.js
+++ external.js
@@ -1,44 +1,36 @@
+const body = document.querySelector('body');
+const grid = document.querySelector('.grid');
+let squareSize = 8;
+const button = document.querySelector('button')
+//button.addEventListener('click', changeSize);
+createGrid(squareSize);
+
 function changeColor(event){
     const select = event.target;
     select.classList.add("black");
 }
-function changeSize(){
-    numofsquare = prompt();
-    console.log(numofsquare);
-    let newsize = ((width / numofsquare) / 2) - 3; 
-    while(grid.hasChildNodes()){
-        grid.removeChild(grid.firstChild);
+function reset(){
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild);
     }
-    for(let k = 0; k < numofsquare; k++){
-        for(let m = 0; m < numofsquare; m++){
-            const square = document.createElement('div');
-            square.classList.add("square");
-            square.addEventListener('mouseover',changeColor);
-            square.style.cssText = `padding: ${Math.round(newsize)}px`;
-            grid.appendChild(square);
+    createGrid(squareSize);
+}
+function createGrid(size){
+    console.log(size);
+    
+    let boxsize = grid.clientWidth / size;
+
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            const box = document.createElement('div');
+            box.classList.add('square')
+            box.style.width = `${boxsize}px`;
+            box.style.height = `${boxsize}px`;
+            grid.appendChild(box);
         }
     }
     
 }
 
 
-const body = document.querySelector('body');
-const grid = document.querySelector('.grid');
-let size = 43.2;
-var numofsquare = 16;
-var width = size * numofsquare 
-grid.style.cssText = `max-width: ${Math.ceil(width)}px`
-const button = document.querySelector('button')
-button.addEventListener('click', changeSize);
-for(let i = 0; i < numofsquare; i++){
-    for(let j = 0; j < numofsquare; j++){
-        const square = document.createElement('div');
-        square.classList.add("square");
-        square.addEventListener('mouseover',changeColor);
-        grid.appendChild(square);
-    }
-    
-    //grid.appendChild(linebreak);
-    //square.textContent = `${i}`;
-}
 
